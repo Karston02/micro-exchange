1990c5f1eda69aa704c966690dd108d40ca9d4eb
Karston Kuciemba <kuciembakarston@gmail.com>
2025-11-25 16:09:56 -0600

    add backend prep

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from api import exchange_router
+
+# create the app
 app = FastAPI()
+
+# set up CORS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+# configure middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# include routers
+app.include_router(exchange_router)
 
 @app.get("/health")
 def health():
